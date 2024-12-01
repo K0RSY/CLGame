a1be7f53513a683e4c01b8b8113e36abecba2d85
K0RSY <pgeorg2010@gmail.com>
2024-12-01 12:01:06 +0200

    Made some changes

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+    
 setup(
   name='CLGame',
   version='1.0',
   author='Korsy',
   author_email='pgeorg@gmail.com',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/K0RSY/CLGame',
   packages=['CLGame'],
   install_requires=['playsound>=1.3.0', 'pynput>=1.7.7'],
   python_requires='>=3.10'
